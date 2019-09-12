a29f2592ad81f29f9e0567f7ed4ed2aa830d5f02
Natsuko YOSHIGA <2604408+nikolito@users.noreply.github.com>
2019-09-13 08:14:26 +0900

    Update ogiTermsList.php

--- userDic/ogiTermsList.php
+++ userDic/ogiTermsList.php
@@ -1,37 +1,31 @@
-<?php  //words/jinmeiPatterns.csvからMecab辞書用データを作成する．
-$data1 = file("/home/yoshiga/ogiNikki/userDic/terms.csv", FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
-$data3 = file("/home/yoshiga/ogiNikki/userDic/ogiTerms.csv", FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
-#print_r($data1); print_r($data2); exit;
+<?php 
+include_once "settings.php";．
+$data1 = file($workDir."userDic/terms.csv", FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+$data3 = file($workDir."userDic/ogiTerms.csv", FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
 
 foreach ($data1 as $d1val) {
 	$d1valArray = explode(",", $d1val);
 	$data1List[] = $d1valArray[0];
-	#$list[] = $data1List[0].",4785,4785,7996,名詞,普通名詞,一般,*,*,*,,".$data1List[0].",".$data1List[0].",,".$data1List[0].",,和,*,*,*,*,OGI_TERMS";
 	$list[] = $data1List[0].",,,,名詞,普通名詞,一般,*,*,*,,".$data1List[0].",".$data1List[0].",,".$data1List[0].",,和,*,*,*,*,OGI_TERMS"."\n";
 }
 
 foreach ($data3 as $val) {
-	$valcomma = explode(",", $val);
-	#print $valcomma[0];
+	$valArray = explode(",", $val);
 	
-	if ($valcomma[5] != "*") {
-		Switch ($valcomma[1]) {
+	if ($valArray[5] != "*") {
+		Switch ($valArray[1]) {
 			case "接頭辞":
-				#$list[] = $valcomma[0].",5688,5688,5580,".$valcomma[1].",".$valcomma[2].",".$valcomma[3].",*,*,*,,".$valcomma[0].",".$valcomma[0].",,".$valcomma[0].",,和,*,*,*,*,OGI_TERMS";
-				$list[] = $valcomma[0].",,,,".$valcomma[1].",".$valcomma[2].",".$valcomma[3].",*,*,*,,".$valcomma[0].",".$valcomma[0].",,".$valcomma[0].",,和,*,*,*,*,OGI_TERMS"."\n";
+				$list[] = $valArray[0].",,,,".$valArray[1].",".$valArray[2].",".$valArray[3].",*,*,*,,".$valArray[0].",".$valArray[0].",,".$valArray[0].",,和,*,*,*,*,OGI_TERMS"."\n";
 				break;
 			case "接尾辞":
-				#$list[] = $valcomma[0].",5771,5771,7129,".$valcomma[1].",".$valcomma[2].",".$valcomma[3].",*,*,*,,".$valcomma[0].",".$valcomma[0].",,".$valcomma[0].",,和,*,*,*,*,OGI_TERMS";
-				$list[] = $valcomma[0].",,,,".$valcomma[1].",".$valcomma[2].",".$valcomma[3].",*,*,*,,".$valcomma[0].",".$valcomma[0].",,".$valcomma[0].",,和,*,*,*,*,OGI_TERMS"."\n";
+				$list[] = $valArray[0].",,,,".$valArray[1].",".$valArray[2].",".$valArray[3].",*,*,*,,".$valArray[0].",".$valArray[0].",,".$valArray[0].",,和,*,*,*,*,OGI_TERMS"."\n";
 				break;
 			case "助詞":
-				#$list[] = $valcomma[0].",803,803,6933,".$valcomma[1].",".$valcomma[2].",".$valcomma[3].",*,*,*,,".$valcomma[0].",".$valcomma[0].",,".$valcomma[0].",,和,*,*,*,*,OGI_TERMS";
-				$list[] = $valcomma[0].",,,,".$valcomma[1].",".$valcomma[2].",".$valcomma[3].",*,*,*,,".$valcomma[0].",".$valcomma[0].",,".$valcomma[0].",,和,*,*,*,*,OGI_TERMS"."\n";
+				$list[] = $valArray[0].",,,,".$valArray[1].",".$valArray[2].",".$valArray[3].",*,*,*,,".$valArray[0].",".$valArray[0].",,".$valArray[0].",,和,*,*,*,*,OGI_TERMS"."\n";
 				break;
 			case "名詞":
 			default:
-				#$list[] = $valcomma[0].",4785,4785,7996,".$valcomma[1].",".$valcomma[2].",".$valcomma[3].",*,*,*,,".$valcomma[0].",".$valcomma[0].",,".$valcomma[0].",,和,*,*,*,*,OGI_TERMS";
-				$list[] = $valcomma[0].",,,,".$valcomma[1].",".$valcomma[2].",".$valcomma[3].",*,*,*,,".$valcomma[0].",".$valcomma[0].",,".$valcomma[0].",,和,*,*,*,*,OGI_TERMS"."\n";
+				$list[] = $valArray[0].",,,,".$valArray[1].",".$valArray[2].",".$valArray[3].",*,*,*,,".$valArray[0].",".$valArray[0].",,".$valArray[0].",,和,*,*,*,*,OGI_TERMS"."\n";
 				break;
 		}
 	}
@@ -41,10 +35,10 @@ sort($list, SORT_NATURAL);
 $list2 = array_unique($list);
 
 	
-$datafile = '/home/yoshiga/ogiNikki/userDic/ogiTermsNoC.csv';
+$datafile = $workDir."userDic/ogiTermsNoC.csv";
 if (is_writable($datafile)) {
 	file_put_contents($datafile, $list2, LOCK_EX);
 } else {
-	trigger_error("ファイルを開ける状態ではありません。");
+	trigger_error("Failed to open the file.");
 }
 ?>
