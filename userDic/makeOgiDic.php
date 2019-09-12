209afc6d285bd5b4a584642d5aacc62e2c3ea103
Natsuko YOSHIGA <2604408+nikolito@users.noreply.github.com>
2019-09-12 20:56:26 +0900

    Update makeOgiDic.php

--- userDic/makeOgiDic.php
+++ userDic/makeOgiDic.php
@@ -2,13 +2,13 @@
 	// makeOgiDic.php: MeCab user dictionary creation
 	// Creating and updating entity lists.
 	
-	//単語の重複除去
 	$workDir = "/your-directory/ogiNikki/";
 	$mecabDir = "/your-mecab-directory/";
 	
+        // Removing dubbed words.
 	system("/usr/bin/php ".$workDir."uniqFile.php;");
 	
-	//MeCabコスト無しファイルの生成
+	//Creating MeCab cost file without cost values.
 	system("/usr/bin/php ".$workDir."userDic/ogiDateList.php;");
 	system("/usr/bin/php ".$workDir."userDic/ogiJinmeiList.php;");
 	system("/usr/bin/php ".$workDir."userDic/ogiEventList.php;");
@@ -19,18 +19,17 @@
 
 	system("/bin/cat ".$workDir."userDic/*NoC.csv > ".$workDir."userDic/ogiNoCAll.csv;");
 	
-	//コスト有りファイル生成
+	// Adding cost values into the cost file.
 	system("/usr/local/libexec/mecab/mecab-dict-index -m ".$mecabDir."model/model.def -d ".$mecabDir." -u ".$workDir."userDic/ogiCost.csv -f utf-8 -t utf-8 -a ".$workDir."userDic/ogiNoCAll.csv;");
 	
-	//コスト調整
+	// In addition of the above operation, modifying the cost values of named entities.
 	$costbase = file($workDir."userDic/ogiCost.csv", FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
 	foreach($costbase as $cbVal) {
 		$dbattr = $cblist = array();
 		$cbattr = explode(",", $cbVal);
 
-		//固有表現クラス別コストの調整 2019-07-07
+		// Longer entities, adding bigger values.
 		$weight = mb_strlen($cbattr[0]);
-		
 		
 		if ($cbattr[21] == "OGI_JINMEI") {
 			$cblist = array_replace($cbattr, array(3 => ($cbattr[3] - 5000 * $weight)));
@@ -53,11 +52,11 @@
 	if (is_writable($datafile)) {
 		file_put_contents($datafile, $changedCost, LOCK_EX);
 	} else {
-		trigger_error("ファイルを開ける状態ではありません。");
+		trigger_error("Failed to open the file. ");
 		exit;
 	}	
 	
-	//ユーザ辞書生成
+	// Creating a user dictionary.
 	system("/usr/local/libexec/mecab/mecab-dict-index -d ".$mecabDir." -u ".$workDir."userDic/ogi.dic -f utf8 -t utf8 ".$workDir."userDic/ogiCostNew.csv;");
 	exit;
 	
