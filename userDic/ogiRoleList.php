3a512cb7c369d519de23ff57b156a4be54b19f6e
Natsuko YOSHIGA <2604408+nikolito@users.noreply.github.com>
2019-09-13 08:08:32 +0900

    Update ogiRoleList.php

--- userDic/ogiRoleList.php
+++ userDic/ogiRoleList.php
@@ -1,18 +1,17 @@
-<?php //Genreのコストファイル生成
-$data1 = file("/home/yoshiga/ogiNikki/userDic/role.csv", FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
-$data2 = file("/home/yoshiga/ogiNikki/userDic/roleSaga.csv", FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+<?php 
+include_once "settings.php";
+$data1 = file($wordDir."userDic/role.csv", FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+$data2 = file($wordDir."userDic/roleSaga.csv", FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
 $data = array_merge($data1, $data2);
 sort($data);
 $dataAll = array_unique($data);
 
 foreach ($dataAll as $val) {
-	$valcomma = explode(",", $val);
-	#print $valcomma[0];
-	#$list[] =  $valcomma[0].",4785,4785,7996,名詞,普通名詞,一般,*,*,*,,,".$valcomma[0].",,".$valcomma[0].",,和,*,*,*,*,OGI_ROLE";
-	$list[] =  $valcomma[0].",,,,名詞,普通名詞,一般,*,*,*,,,".$valcomma[0].",,".$valcomma[0].",,和,*,*,*,*,OGI_ROLE"."\n";
+	$valArray = explode(",", $val);
+	$list[] =  $valArray[0].",,,,名詞,普通名詞,一般,*,*,*,,,".$valArray[0].",,".$valArray[0].",,和,*,*,*,*,OGI_ROLE"."\n";
 }
 
-$datafile = "/home/yoshiga/ogiNikki/userDic/ogiRoleNoC.csv";
+$datafile = $wordDir."userDic/ogiRoleNoC.csv";
 $dataval = '';
 if (is_writable($datafile)) {
 	file_put_contents($datafile, $list, LOCK_EX);
