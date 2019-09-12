12dc3cd54637975314dc2f12a647239bbfc674bd
Natsuko YOSHIGA <2604408+nikolito@users.noreply.github.com>
2019-09-12 17:41:47 +0900

    Update uniqFile.php

--- userDic/uniqFile.php
+++ userDic/uniqFile.php
@@ -1,17 +1,12 @@
 <?php
-	include_once "/home/yoshiga/ogiNikki/function.php";
-	sortUniqueFile("/home/yoshiga/ogiNikki/userDic/date.csv");
-	sortUniqueFile("/home/yoshiga/ogiNikki/userDic/place.csv");
-	sortUniqueFile("/home/yoshiga/ogiNikki/userDic/jinmei.csv");
-	sortUniqueFile("/home/yoshiga/ogiNikki/userDic/role.csv");
-	sortUniqueFile("/home/yoshiga/ogiNikki/userDic/event.csv");
-	sortUniqueFile("/home/yoshiga/ogiNikki/userDic/terms.csv");
-	sortUniqueFile("/home/yoshiga/ogiNikki/userDic/quantity.csv");
-	sortUniqueFile("/home/yoshiga/ogiNikki/userDic/placeJisha.csv");
-	#sortUniqueFile("/home/yoshiga/ogiNikki/userDic/placeEx.csv");
-	#sortUniqueFile("/home/yoshiga/ogiNikki/userDic/jinmeiEx.csv");
-	#sortUniqueFile("/home/yoshiga/ogiNikki/userDic/roleEx.csv");
-	#sortUniqueFile("/home/yoshiga/ogiNikki/userDic/genreEx.csv");
-	#sortUniqueFile("/home/yoshiga/ogiNikki/userDic/termsEx.csv");
-	#sortUniqueFile("/home/yoshiga/ogiNikki/userDic/etcEx.csv");
+	$workDir = "/your-directory/ogiNikki/";
+	include_once $workDir."function.php";
+	sortUniqueFile($workDir."userDic/date.csv");
+	sortUniqueFile($workDir."userDic/place.csv");
+	sortUniqueFile($workDir."userDic/jinmei.csv");
+	sortUniqueFile($workDir."userDic/role.csv");
+	sortUniqueFile($workDir."userDic/event.csv");
+	sortUniqueFile($workDir."userDic/terms.csv");
+	sortUniqueFile($workDir."userDic/quantity.csv");
+	sortUniqueFile($workDir."userDic/placeJisha.csv");
 ?>
