28e0a1ac77cc58eda19f141e5cf68cf3d74f45e1
Natsuko YOSHIGA <2604408+nikolito@users.noreply.github.com>
2019-09-13 07:01:26 +0900

    Update ogiPlaceList.php

--- userDic/ogiPlaceList.php
+++ userDic/ogiPlaceList.php
@@ -1,21 +1,20 @@
 <?php
-$data1 = file("/home/yoshiga/ogiNikki/userDic/place.csv", FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
-$data2 = file("/home/yoshiga/ogiNikki/userDic/placeSaga.csv", FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
-$data3 = file("/home/yoshiga/ogiNikki/userDic/placeChakuto.csv", FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
-$data4 = file("/home/yoshiga/ogiNikki/userDic/placeSagaJisha.csv", FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
-$data5 = file("/home/yoshiga/ogiNikki/userDic/placeJisha.csv", FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+$wordDir = "/home/yoshiga/ogiNikki/";
+$data1 = file($wordDir."userDic/place.csv", FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+$data2 = file($wordDir."userDic/placeSaga.csv", FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+$data3 = file($wordDir."userDic/placeChakuto.csv", FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+$data4 = file($wordDir."userDic/placeSagaJisha.csv", FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+$data5 = file($wordDir."userDic/placeJisha.csv", FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
 $dataAll = array_merge($data1, $data2, $data3, $data4, $data5);
 sort($dataAll, SORT_NATURAL);
 $data = array_unique($dataAll);
 
 foreach ($data as $val) {
-	$valcomma = explode(",", $val);
-	#print $valcomma[0];
-	#$list[] =  $valcomma[0].",4792,4792,10466,名詞,固有名詞,地名,一般,*,*,,,".$valcomma[0].",,".$valcomma[0].",,固,*,*,*,*,OGI_PLACETERMS";
-	$list[] =  $valcomma[0].",,,,名詞,固有名詞,地名,一般,*,*,,,".$valcomma[0].",,".$valcomma[0].",,固,*,*,*,*,OGI_PLACE"."\n";
+	$valArray = explode(",", $val);
+	$list[] =  $valArray[0].",,,,名詞,固有名詞,地名,一般,*,*,,,".$valArray[0].",,".$valArray[0].",,固,*,*,*,*,OGI_PLACE"."\n";
 }
 
-$datafile = '/home/yoshiga/ogiNikki/userDic/ogiPlaceNoC.csv';
+$datafile = '$wordDir."userDic/ogiPlaceNoC.csv';
 $dataval = '';
 if (is_writable($datafile)) {
 	file_put_contents($datafile, $list, LOCK_EX);
