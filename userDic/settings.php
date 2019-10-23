187480bafa78e6cbc11c8f0ed3f1428e754d86bc
Natsuko YOSHIGA <2604408+nikolito@users.noreply.github.com>
2019-10-23 17:23:37 +0900

    Update settings.php

--- userDic/settings.php
+++ userDic/settings.php
@@ -1,5 +1,5 @@
 <?php
 // Settings for all files under userDic directory.
-$wordDir = "/your-directory/ogiNikki/";
+$workDir = "/your-directory/ogiNikki/";
 $mecabDir = "/your-mecab-directory/";
 ?>
